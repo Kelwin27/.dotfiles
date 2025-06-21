4d5439a228293c4b256fe9516efdddc5ad8f1178
Frost-Phoenix <67cyril6767@gmail.com>
2025-06-21 15:58:59 +0300

    waybar top

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "FrostPhoenix's nixos configuration";
+  description = "KelWin's fork of FrostPhoenix's nixos configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
