a3b8f5bc72daa2e04fe174b45ada7eea340c27cd
kelwin <kelwin90210@gmail.com>
2025-10-16 16:23:16 +0300

    feat: add nvidia latest

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -3,6 +3,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.supportedFilesystems = [ "ntfs" ];
 }
