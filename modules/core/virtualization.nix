8b72e621cd4c0b19f4efee030b4262b8c42fd600
kelwin <kelwin90210@gmail.com>
2025-10-27 15:23:56 +0300

    feat: add docker

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -1,31 +1,10 @@
-{ pkgs, username, ... }:
+{ ... }:
 {
-  # Add user to libvirtd group
-  users.users.${username}.extraGroups = [ "libvirtd" ];
-
-  # Install necessary packages
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
-    spice
-    spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-    adwaita-icon-theme
-  ];
-
-  # Manage the virtualisation services
-  virtualisation = {
-    libvirtd = {
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
       enable = true;
-      qemu = {
-        swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-      };
+      setSocketVariable = true;
     };
-    spiceUSBRedirection.enable = true;
   };
-  services.spice-vdagentd.enable = true;
 }
